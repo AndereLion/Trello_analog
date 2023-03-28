137c596b3c26f55f7e8eb9abb40a334ebf19f91b
Roman Senyshyn <senjaskorp@gmail.com>
2023-03-28 20:07:16 +0300

    Added SECRET_KEY to environ, Changed debug to False,ALLOWED_HOSTS = ["127.0.0.1"]

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -19,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d!9lm(hgwx#s9v(0iq0km9@m*&t@e@*h5k+gx^2&f#6tc%4z%-"
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-d!9lm(hgwx#s9v(0iq0km9@m*&t@e@*h5k+gx^2&f#6tc%4z%-")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
