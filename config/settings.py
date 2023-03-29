dc3cf7ab4cdec2321c566b6459ce1cadc609d7fa
Roman Senyshyn <senjaskorp@gmail.com>
2023-03-29 14:06:25 +0300

    Added build.sh, updated requirements.txt, settings.py

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-d!9lm(hgwx#s9v(0iq0km9@m*&t@e@*h5k+gx^2&f#6tc%4z%-")
 
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
